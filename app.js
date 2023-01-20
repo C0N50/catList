0d9b5a8f620e5051b332ad2c1f5585bc8c984379
Connor Ferris <ferr0373@gmail.com>
2023-01-20 15:01:18 -0600

    finished cat page

--- app.js
+++ app.js
@@ -5,10 +5,14 @@ function onReady () {
    // setInterval(hello, 10000);
    render();
 
+   //handle addCatForm
    $('#addCatForm').on('submit', onAddCat);
+
+   //handle deleteCatBtn - Make sure to listen to parent elements that are not altered in a render.
+   $('#cat-table').on('click', '.deleteCatBtn', onDeleteCat);
 }
 
-// State
+// State Variables
 
 let cats = [
     {name : 'Xiu Xiu', color : 'Brown', age : 4, tabi : true},
@@ -22,6 +26,8 @@ let cats = [
 function render() {
     console.log('in render()');
 
+    $('#cat-table').empty();
+
     for (let cat of cats) {
         $('#cat-table').append(
             `<tr>
@@ -29,6 +35,7 @@ function render() {
                 <td>${cat.color}</td> 
                 <td>${cat.age}</td> 
                 <td>${cat.tabi}</td>
+                <td> <button class='deleteCatBtn'> Delete Cat 😿 </button> </td>
             </tr>`);
     }
 }
@@ -38,20 +45,36 @@ function onAddCat(evt) {
     evt.preventDefault();
     console.log('In onAddCat');
 
+    //create new cat object
     let cat = {
         name : $('#nameInput').val(),
         color : $('#colorInput').val(),
         age : Number($('#ageInput').val()),
         tabi : $('#tabiInput').is(":checked")
-    }
+    };
     
+    //push cat to the array
     cats.push(cat);
     console.log(cats);
 
-    $('#cat-table').empty();
+    //render new page
     render();
 }
 
+function onDeleteCat() {
+    console.log('onDeleteCat');
+    //$(this).parent().parent().remove();
+
+        //       btn    td      tr
+    let myTR = $(this).parent().parent();
+    let indexOFCat = myTR.index();
+    console.log(('indexOfCat'), indexOFCat);
+
+    cats.splice(indexOFCat, 1); // 👋🐈
+
+    console.log('cats after removal', cats);
+    render();
+}
 
 
 
